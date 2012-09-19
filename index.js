e9a0fb54ee3dcd71c11a353dc8e80637a33c7a08
TJ Holowaychuk <tj@vision-media.ca>
2012-09-19 10:54:33 -0700

    add stringtoFunction

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 
 module.exports = function(arr, fn){
   var ret = [];
+  fn = toFunction(fn);
   for (var i = 0; i < arr.length; ++i) {
     ret.push(fn(arr[i], i));
   }
   return ret;
 };
+
+function toFunction(obj) {
+  switch (typeof obj) {
+    case 'function':
+      return obj;
+    case 'string':
+      return stringToFunction(obj);
+    default:
+      throw new TypeError('invalid callback "' + obj + '"');
+  }
+}
+
+function stringToFunction(str) {
+  return function(obj){
+    return obj[str];
+  }
+}
--- test/index.js
+++ test/index.js
@@ -1,6 +1,10 @@
 
 var map = require('..');
 
+var loki = { name: 'loki', age: 1 };
+var tobi = { name: 'tobi', age: 2 };
+var jane = { name: 'jane', age: 8 };
+
 describe('map(arr, fn)', function(){
   it('should map values', function(){
     var arr = [1,2,3];
@@ -12,5 +16,11 @@ describe('map(arr, fn)', function(){
     var arr = [1,2,3];
     arr = map(arr, function(n, i){ return i });
     arr.should.eql([0, 1, 2]);
+  })
+
+  it('should support property strings', function(){
+    var users = [tobi, loki, jane];
+    var arr = map(users, 'name');
+    arr.should.eql(['tobi', 'loki', 'jane']);
   })
 })
