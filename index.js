a33e131c24495d1f363c12118e263e41353d1109
TJ Holowaychuk <tj@vision-media.ca>
2012-09-20 10:17:36 -0700

    add to-function dep

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 
 /**
+ * Module dependencies.
+ */
+
+var toFunction = require('to-function');
+
+/**
  * Map the given `arr` with callback `fn(val, i)`.
  *
  * @param {Array} arr
@@ -16,41 +22,3 @@ module.exports = function(arr, fn){
   }
   return ret;
 };
-
-/**
- * Convert `obj` to a `Function`.
- *
- * @param {Mixed} obj
- * @return {Function}
- * @api private
- */
-
-function toFunction(obj) {
-  switch (typeof obj) {
-    case 'function':
-      return obj;
-    case 'string':
-      return stringToFunction(obj);
-    default:
-      throw new TypeError('invalid callback "' + obj + '"');
-  }
-}
-
-/**
- * Convert property `str` to a function.
- *
- * @param {String} str
- * @return {Function}
- * @api private
- */
-
-function stringToFunction(str) {
-  var props = str.split('.');
-  return function(obj){
-    for (var i = 0; i < props.length; ++i) {
-      if (null == obj) return;
-      obj = obj[props[i]];
-    }
-    return obj;
-  }
-}
